a25fb631983f617bce79f87c527e4a7822ceb5b7
cassidypignatello <cassidy.pignatello@gmail.com>
2014-10-28 18:36:00 -0400

    passes in concert info dynamically and creates controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,14 @@
 'use strict';
 
-/* Controllers */
+var concertApp = angular.module('concertApp', []);
+
+concertApp.controller('ConcertListCtrl', function($scope) {
+  $scope.concerts = [
+    {'name': 'Slowdive',
+     'snippet': 'Distinctive indie rockers from Reading who employed atmospheric harmonies, rippling processed guitars, and an ambient production sensibility.'},
+    {'name': 'Fleetwood Mac',
+     'snippet': "Mick Fleetwood's boys (and girls) morphed from a respected British blues outfit into the world's biggest pop band by the 1970s."},
+    {'name': 'Caribou',
+     'snippet': "Canadian musician's one-man project (originally called Manitoba) that seamlessly blends electronic production with psychedelic pop pastiche."}
+  ];
+});
